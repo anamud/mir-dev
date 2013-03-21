0d8fcd89af39dcf9bfd9f480f75a6a2daea74b12
Ananya Muddukrishna <ananya@kth.se>
2013-03-21 09:01:55 +0100

    Changed tag gen

--- src/gentags.sh
+++ src/gentags.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-
+# Works great with autotag.vim plugin
 ctags -R .
+
+# Cscope is deprecated/un-usable
 #ackack -n --cc -f > cscope.files
 #ctags -L cscope.files
 #ctags -e -L cscope.files 
