d98e5973d1eecf7882faafea8be4a1696758f5e1
Ananya Muddukrishna <ananya@kth.se>
2013-10-07 18:19:00 +0200

    Minor changes

--- scripts/collect-prof-data.sh
+++ scripts/collect-prof-data.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-find $MIR_ROOT -name '*graph*' -o -name 'profile.s*' -o -name '*_stats*' -print0 | tar -czvf prof-data.tar.gz -T - --null
+find $MIR_ROOT/test -name "*graph*" -o -name "profile.s*" -o -name "*_stats" > prof-data-files.txt
+tar -czvf prof-data.tgz --files-from=prof-data-files.txt
