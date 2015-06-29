6ac163a47217e6ae742886733f1829889f71cedc
Ananya Muddukrishna <ananya@kth.se>
2015-06-29 15:48:46 +0200

    Fibonacci program test case.

---
+++ tests/test_2/test_2.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+#include <check.h>
+#include <omp.h>
+#include "mir_public_int.h"
+
+static uint64_t fib_seq(int n)
+{ /*{{{*/
+    if (n < 2)
+        return n;
+    return fib_seq(n - 1) + fib_seq(n - 2);
+} /*}}}*/
+
+static uint64_t fib(int n, int d, int cutoff)
+{ /*{{{*/
+    uint64_t x, y;
+
+    if (n < 2)
+        return n;
+
+    if (d < cutoff)
+    {
+#pragma omp task shared(x) firstprivate(n, d)
+        x = fib(n - 1, d + 1, cutoff);
+
+#pragma omp task shared(y) firstprivate(n, d)
+        y = fib(n - 2, d + 1, cutoff);
+
+#pragma omp taskwait
+    }
+    else
+    {
+        x = fib_seq(n - 1);
+        y = fib_seq(n - 2);
+    }
+
+    return x + y;
+} /*}}}*/
+
+/* Fibonacci program OpenMP tasks. */
+START_TEST(fib_omp)
+{
+    int result;
+    int n = 42;
+    int cutoff = 12;
+
+#pragma omp parallel
+    {
+/* Single works as specified in the standard. */
+#pragma omp single
+        {
+#pragma omp task firstprivate(n, cutoff)
+            {
+                result = fib(n, 0, cutoff);
+            }
+#pragma omp taskwait
+        }
+    }
+
+    /* fib(42) == 267914296 */
+    ck_assert_int_eq(result, 267914296);
+}
+END_TEST
+
+Suite* test_suite(void)
+{
+    Suite* s;
+    s = suite_create("Test_2");
+
+    /* Fibonacci omp tasks test case */
+    TCase* tc_fib_omp;
+    tc_fib_omp = tcase_create("Fib_omp");
+    tcase_add_test(tc_fib_omp, fib_omp);
+    suite_add_tcase(s, tc_fib_omp);
+
+    return s;
+}
+
+int main(void)
+{
+    int number_failed;
+    Suite* s;
+    SRunner* sr;
+
+    s = test_suite();
+    sr = srunner_create(s);
+
+    srunner_run_all(sr, CK_VERBOSE);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
