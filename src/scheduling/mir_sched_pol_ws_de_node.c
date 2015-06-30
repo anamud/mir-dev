928e3fea76d08806da12dc71ac443d2a0743f695
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:41:59 +0200

    Indentation and stripped white space.

--- src/scheduling/mir_sched_pol_ws_de_node.c
+++ src/scheduling/mir_sched_pol_ws_de_node.c
@@ -10,7 +10,7 @@
 #include "mir_defines.h"
 #ifdef MIR_MEM_POL_ENABLE
 #include "mir_mem_pol.h"
-#endif 
+#endif
 
 #include <stdbool.h>
 #include <stdint.h>
@@ -20,32 +20,30 @@
 extern uint32_t g_num_tasks_waiting;
 extern struct mir_runtime_t* runtime;
 
-void create_ws_de_node ()
-{/*{{{*/
+void create_ws_de_node()
+{ /*{{{*/
     struct mir_sched_pol_t* sp = runtime->sched_pol;
     MIR_ASSERT(NULL != sp);
 
     // Create worker private task queues
     sp->num_queues = runtime->num_workers;
-    sp->queues = (struct mir_queue_t**) mir_malloc_int (sp->num_queues * sizeof(mir_dequeue_t*));
+    sp->queues = (struct mir_queue_t**)mir_malloc_int(sp->num_queues * sizeof(mir_dequeue_t*));
     MIR_ASSERT(NULL != sp->queues);
 
-    for(int i=0; i< sp->num_queues; i++)
-    {
-        sp->queues[i] = (struct mir_queue_t*) newWSDeque(sp->queue_capacity);
+    for (int i = 0; i < sp->num_queues; i++) {
+        sp->queues[i] = (struct mir_queue_t*)newWSDeque(sp->queue_capacity);
         MIR_ASSERT(NULL != sp->queues[i]);
     }
-}/*}}}*/
+} /*}}}*/
 
-void destroy_ws_de_node ()
-{/*{{{*/
+void destroy_ws_de_node()
+{           /*{{{*/
     return; // FIXME: Bad exit. Workers bang on queues which are freed.
     struct mir_sched_pol_t* sp = runtime->sched_pol;
     MIR_ASSERT(NULL != sp);
 
     // Free queues
-    for(int i=0; i<sp->num_queues ; i++)
-    {
+    for (int i = 0; i < sp->num_queues; i++) {
         MIR_ASSERT(NULL != sp->queues[i]);
         freeWSDeque((mir_dequeue_t*)sp->queues[i]);
         sp->queues[i] = NULL;
@@ -54,76 +52,69 @@ void destroy_ws_de_node ()
     MIR_ASSERT(NULL != sp->queues);
     mir_free_int(sp->queues, sizeof(mir_dequeue_t*) * sp->num_queues);
     sp->queues = NULL;
-}/*}}}*/
+} /*}}}*/
 
-int push_ws_de_node (struct mir_worker_t* worker, struct mir_task_t* task)
-{/*{{{*/
+int push_ws_de_node(struct mir_worker_t* worker, struct mir_task_t* task)
+{ /*{{{*/
     MIR_ASSERT(NULL != task);
     MIR_ASSERT(NULL != worker);
 
     int pushed = 1;
 
     // ws has per-worker queues
-    mir_dequeue_t* queue = (mir_dequeue_t*) runtime->sched_pol->queues[worker->id];
+    mir_dequeue_t* queue = (mir_dequeue_t*)runtime->sched_pol->queues[worker->id];
     MIR_ASSERT(NULL != queue);
-    if( rtsFalse == pushWSDeque(queue, (void*) task) )
-    {
-#ifdef MIR_INLINE_TASK_IF_QUEUE_FULL 
+    if (rtsFalse == pushWSDeque(queue, (void*)task)) {
+#ifdef MIR_INLINE_TASK_IF_QUEUE_FULL
         pushed = 0;
         mir_task_execute(task);
         // Update stats
-        if(runtime->enable_worker_stats == 1)
+        if (runtime->enable_worker_stats == 1)
             worker->statistics->num_tasks_inlined++;
 #else
         MIR_ABORT(MIR_ERROR_STR "Cannot enqueue task. Increase queue capacity using MIR_CONF.\n");
 #endif
     }
-    else
-    {
+    else {
         __sync_fetch_and_add(&g_num_tasks_waiting, 1);
         // Update stats
-        if(runtime->enable_worker_stats == 1)
+        if (runtime->enable_worker_stats == 1)
             worker->statistics->num_tasks_created++;
     }
 
     //MIR_RECORDER_STATE_END(NULL, 0);
 
     return pushed;
-}/*}}}*/
+} /*}}}*/
 
-int pop_ws_de_node (struct mir_task_t** task)
-{/*{{{*/
+int pop_ws_de_node(struct mir_task_t** task)
+{ /*{{{*/
     int found = 0;
     struct mir_sched_pol_t* sp = runtime->sched_pol;
     MIR_ASSERT(NULL != sp);
     uint32_t num_queues = sp->num_queues;
-    struct mir_worker_t* worker = mir_worker_get_context(); 
+    struct mir_worker_t* worker = mir_worker_get_context();
     MIR_ASSERT(NULL != worker);
     uint16_t node = runtime->arch->node_of(worker->cpu_id);
 
     // First try to pop from own queue
     //MIR_RECORDER_STATE_BEGIN(MIR_STATE_TPOP);
 
-    mir_dequeue_t* queue = (mir_dequeue_t*) sp->queues[worker->id];
-    if(looksEmptyWSDeque(queue) == rtsFalse)
-    {
+    mir_dequeue_t* queue = (mir_dequeue_t*)sp->queues[worker->id];
+    if (looksEmptyWSDeque(queue) == rtsFalse) {
         *task = NULL;
-        *task = (struct mir_task_t*) popWSDeque(queue);
-        if(*task)
-        {
+        *task = (struct mir_task_t*)popWSDeque(queue);
+        if (*task) {
             bool grab = __sync_bool_compare_and_swap(&((*task)->taken), 0, 1);
-            if(grab == true)
-            {
+            if (grab == true) {
                 // Update stats
-                if(runtime->enable_worker_stats == 1)
-                {
+                if (runtime->enable_worker_stats == 1) {
 #ifdef MIR_MEM_POL_ENABLE
                     struct mir_mem_node_dist_t* dist = mir_task_get_mem_node_dist(*task, MIR_DATA_ACCESS_READ);
-                    if(dist)
-                    {
+                    if (dist) {
                         /*MIR_INFORM("Dist for task %" MIR_FORMSPEC_UL ": ", (*task)->id.uid);*/
                         /*for(int i=0; i<runtime->arch->num_nodes; i++)*/
-                            /*MIR_INFORM("%lu ", dist->buf[i]);*/
+                        /*MIR_INFORM("%lu ", dist->buf[i]);*/
                         /*MIR_INFORM("\n");*/
 
                         (*task)->comm_cost = mir_mem_node_dist_get_comm_cost(dist, node);
@@ -151,39 +142,34 @@ int pop_ws_de_node (struct mir_task_t** task)
     // Next try to pop from other queues within own node
 
     uint16_t ctr = worker->id + 1;
-    if(ctr == num_queues) ctr = 0;
+    if (ctr == num_queues)
+        ctr = 0;
 
-    while(ctr != worker->id)
-    {
+    while (ctr != worker->id) {
         uint16_t victim_node = runtime->arch->node_of(ctr);
-        if(node != victim_node)
-        {
+        if (node != victim_node) {
             // Incremenet counter and wrap around
             ctr++;
-            if(ctr == num_queues) ctr = 0;
+            if (ctr == num_queues)
+                ctr = 0;
             continue;
         }
 
-        mir_dequeue_t* queue = (mir_dequeue_t*) sp->queues[ctr];
-        if(looksEmptyWSDeque(queue) == rtsFalse)
-        {
+        mir_dequeue_t* queue = (mir_dequeue_t*)sp->queues[ctr];
+        if (looksEmptyWSDeque(queue) == rtsFalse) {
             *task = NULL;
-            *task = (struct mir_task_t*) stealWSDeque(queue);
-            if(*task) 
-            {
+            *task = (struct mir_task_t*)stealWSDeque(queue);
+            if (*task) {
                 bool grab = __sync_bool_compare_and_swap(&((*task)->taken), 0, 1);
-                if(grab == true)
-                {
+                if (grab == true) {
                     // Update stats
-                    if(runtime->enable_worker_stats == 1)
-                    {
+                    if (runtime->enable_worker_stats == 1) {
 #ifdef MIR_MEM_POL_ENABLE
                         struct mir_mem_node_dist_t* dist = mir_task_get_mem_node_dist(*task, MIR_DATA_ACCESS_READ);
-                        if(dist)
-                        {
+                        if (dist) {
                             /*MIR_INFORM("Dist for task %" MIR_FORMSPEC_UL ": ", (*task)->id.uid);*/
                             /*for(int i=0; i<runtime->arch->num_nodes; i++)*/
-                                /*MIR_INFORM("%lu ", dist->buf[i]);*/
+                            /*MIR_INFORM("%lu ", dist->buf[i]);*/
                             /*MIR_INFORM("\n");*/
 
                             (*task)->comm_cost = mir_mem_node_dist_get_comm_cost(dist, node);
@@ -206,49 +192,43 @@ int pop_ws_de_node (struct mir_task_t** task)
 
         // Incremenet counter and wrap around
         ctr++;
-        if(ctr == num_queues) ctr = 0;
+        if (ctr == num_queues)
+            ctr = 0;
     }
 
     if (found)
         return found;
-    
+
     // Next try to pop from other queues within other nodes
-    for(int d=1; d<=runtime->arch->diameter && found!=1; d++)
-    {/*{{{*/
+    for (int d = 1; d <= runtime->arch->diameter && found != 1; d++) { /*{{{*/
         uint16_t neighbors[runtime->arch->num_nodes];
         uint16_t count = runtime->arch->vicinity_of(neighbors, node, d);
-        for(int i=0; i<count && found!=1; i++)
-        {
+        for (int i = 0; i < count && found != 1; i++) {
             struct mir_sbuf_t cpus;
             runtime->arch->cpus_of(&cpus, neighbors[i]);
-            for(int j=0; j<cpus.size; j++)
-            {
-                mir_dequeue_t* queue = (mir_dequeue_t*) sp->queues[cpus.buf[j]];
-                if(looksEmptyWSDeque(queue) == rtsFalse)
-                {
+            for (int j = 0; j < cpus.size; j++) {
+                mir_dequeue_t* queue = (mir_dequeue_t*)sp->queues[cpus.buf[j]];
+                if (looksEmptyWSDeque(queue) == rtsFalse) {
                     *task = NULL;
-                    *task = (struct mir_task_t*) stealWSDeque(queue);
-                    if(*task) 
-                    {
+                    *task = (struct mir_task_t*)stealWSDeque(queue);
+                    if (*task) {
                         bool grab = __sync_bool_compare_and_swap(&((*task)->taken), 0, 1);
-                        if(grab == true)
-                        {
+                        if (grab == true) {
                             // Update stats
-                            if(runtime->enable_worker_stats == 1)
-                            {
-/*#ifdef MIR_MEM_POL_ENABLE*//*{{{*/
+                            if (runtime->enable_worker_stats == 1) {
+                                /*#ifdef MIR_MEM_POL_ENABLE*/ /*{{{*/
                                 /*struct mir_mem_node_dist_t* dist = mir_task_get_mem_node_dist(*task, MIR_DATA_ACCESS_READ);*/
                                 /*if(dist)*/
                                 /*{*/
-                                    /*[>MIR_INFORM("Dist for task %" MIR_FORMSPEC_UL ": ", (*task)->id.uid);<]*/
-                                    /*[>for(int i=0; i<runtime->arch->num_nodes; i++)<]*/
-                                        /*[>MIR_INFORM("%lu ", dist->buf[i]);<]*/
-                                    /*[>MIR_INFORM("\n");<]*/
+                                /*[>MIR_INFORM("Dist for task %" MIR_FORMSPEC_UL ": ", (*task)->id.uid);<]*/
+                                /*[>for(int i=0; i<runtime->arch->num_nodes; i++)<]*/
+                                /*[>MIR_INFORM("%lu ", dist->buf[i]);<]*/
+                                /*[>MIR_INFORM("\n");<]*/
 
-                                    /*(*task)->comm_cost = mir_mem_node_dist_get_comm_cost(dist, node);*/
-                                    /*mir_worker_statistics_update_comm_cost(worker->statistics, (*task)->comm_cost);*/
+                                /*(*task)->comm_cost = mir_mem_node_dist_get_comm_cost(dist, node);*/
+                                /*mir_worker_statistics_update_comm_cost(worker->statistics, (*task)->comm_cost);*/
                                 /*}*/
-/*#endif*//*}}}*/
+                                /*#endif*/ /*}}}*/
 
                                 worker->statistics->num_tasks_stolen++;
                             }
@@ -264,13 +244,12 @@ int pop_ws_de_node (struct mir_task_t** task)
                 }
             }
         }
-    }/*}}}*/
+    } /*}}}*/
 
     return found;
-}/*}}}*/
+} /*}}}*/
 
-struct mir_sched_pol_t policy_ws_de_node = 
-{/*{{{*/
+struct mir_sched_pol_t policy_ws_de_node = { /*{{{*/
     .num_queues = MIR_WORKER_MAX_COUNT,
     .queue_capacity = MIR_QUEUE_MAX_CAPACITY,
     .queues = NULL,
@@ -280,5 +259,5 @@ struct mir_sched_pol_t policy_ws_de_node =
     .destroy = destroy_ws_de_node,
     .push = push_ws_de_node,
     .pop = pop_ws_de_node
-};/*}}}*/
+}; /*}}}*/
 
