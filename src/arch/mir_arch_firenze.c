a2230f297c02dd0d64591c29210916280b00374a
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:30:25 +0200

    Indentation and stripped white space.

--- src/arch/mir_arch_firenze.c
+++ src/arch/mir_arch_firenze.c
@@ -3,69 +3,66 @@
 #include "mir_utils.h"
 
 void create_firenze()
-{/*{{{*/
+{ /*{{{*/
     return;
-}/*}}}*/
+} /*}}}*/
 
 void destroy_firenze()
-{/*{{{*/
+{ /*{{{*/
     return;
-}/*}}}*/
+} /*}}}*/
 
 uint16_t sys_cpu_of_firenze(uint16_t cpuid)
-{/*{{{*/
+{ /*{{{*/
     struct mir_arch_topology_t topology_firenze[] = {
-        {0,0,0,0,0},
-        {1,2,1,0,0}
+        { 0, 0, 0, 0, 0 },
+        { 1, 2, 1, 0, 0 }
     };
     MIR_ASSERT(topology_firenze[cpuid].log_cpu == cpuid);
     return topology_firenze[cpuid].sys_cpu;
-}/*}}}*/
+} /*}}}*/
 
 uint16_t node_of_firenze(uint16_t cpuid)
-{/*{{{*/
-    switch(cpuid)
-    {
-        case 0:
-        case 1:
-            return 0;
-            break;
-        default:
-            MIR_ABORT(MIR_ERROR_STR "Node of CPU not found!\n");
-            break;
+{ /*{{{*/
+    switch (cpuid) {
+    case 0:
+    case 1:
+        return 0;
+        break;
+    default:
+        MIR_ABORT(MIR_ERROR_STR "Node of CPU not found!\n");
+        break;
     }
     return 0;
-}/*}}}*/
+} /*}}}*/
 
 void cpus_of_firenze(struct mir_sbuf_t* cpuids, uint16_t nodeid)
-{/*{{{*/
+{ /*{{{*/
     MIR_ASSERT(cpuids != NULL);
     cpuids->size = 0;
-    switch(nodeid)
-    {
-        case 0:
-            cpuids->size = 2;
-            for(int i=0, basecpu=0; i<2; i++)
-                cpuids->buf[i] = basecpu+i;
-            break;
-        default:
-            MIR_ABORT(MIR_ERROR_STR "CPUs of node not found!\n");
-            break;
+    switch (nodeid) {
+    case 0:
+        cpuids->size = 2;
+        for (int i = 0, basecpu = 0; i < 2; i++)
+            cpuids->buf[i] = basecpu + i;
+        break;
+    default:
+        MIR_ABORT(MIR_ERROR_STR "CPUs of node not found!\n");
+        break;
     }
-}/*}}}*/
+} /*}}}*/
 
 uint16_t vicinity_of_firenze(uint16_t* neighbors, uint16_t nodeid, uint16_t diameter)
-{/*{{{*/
+{ /*{{{*/
     return 0;
-}/*}}}*/
+} /*}}}*/
 
 uint16_t comm_cost_of_firenze(uint16_t from_nodeid, uint16_t to_nodeid)
-{/*{{{*/
+{ /*{{{*/
     return 10;
-}/*}}}*/
+} /*}}}*/
 
-struct mir_arch_t arch_firenze = 
-{
+struct mir_arch_t arch_firenze = {
     .name = "firenze",
     .num_nodes = 1,
     .num_cores = 2,
