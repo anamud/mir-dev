5600fa195a0b57a89df3477105ad32586a19e6f3
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:30:17 +0200

    Indentation and stripped white space.

--- src/arch/mir_arch.h
+++ src/arch/mir_arch.h
@@ -6,34 +6,32 @@
 
 BEGIN_C_DECLS
 
-struct mir_arch_topology_t
-{ 
-    uint16_t log_cpu; 
+struct mir_arch_topology_t {
+    uint16_t log_cpu;
     uint16_t sys_cpu;
     uint16_t core;
     uint16_t socket;
     uint16_t node;
 };
 
-struct mir_arch_t 
-{/*{{{*/
+struct mir_arch_t { /*{{{*/
     // Constants
     const char* name;
     uint16_t num_cores;
     uint16_t num_nodes;
     uint16_t diameter; // Largest hop distance between two nodes
-    size_t llc_size_KB; 
+    size_t llc_size_KB;
 
     // Interfaces
-    void (*config) (const char* conf_str);
-    void (*create) ();
-    void (*destroy) ();
-    uint16_t (*sys_cpu_of) (uint16_t cpuid);
-    uint16_t (*node_of) (uint16_t cpuid);
-    void (*cpus_of) (struct mir_sbuf_t* cpuids, uint16_t nodeid);
-    uint16_t (*vicinity_of) (uint16_t* neighbors, uint16_t nodeid, uint16_t diameter);
-    uint16_t (*comm_cost_of) (uint16_t from_nodeid, uint16_t to_nodeid);
-};/*}}}*/
+    void (*config)(const char* conf_str);
+    void (*create)();
+    void (*destroy)();
+    uint16_t (*sys_cpu_of)(uint16_t cpuid);
+    uint16_t (*node_of)(uint16_t cpuid);
+    void (*cpus_of)(struct mir_sbuf_t* cpuids, uint16_t nodeid);
+    uint16_t (*vicinity_of)(uint16_t* neighbors, uint16_t nodeid, uint16_t diameter);
+    uint16_t (*comm_cost_of)(uint16_t from_nodeid, uint16_t to_nodeid);
+}; /*}}}*/
 
 struct mir_arch_t* mir_arch_create_by_query();
 
