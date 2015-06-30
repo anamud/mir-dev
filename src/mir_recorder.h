689caea9ea86e0511ed504434945a9af9c725bd1
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:29:38 +0200

    Indentation and stripped white space.

--- src/mir_recorder.h
+++ src/mir_recorder.h
@@ -14,12 +14,11 @@
 
 #include "mir_types.h"
 
-BEGIN_C_DECLS 
+BEGIN_C_DECLS
 
 // FIXME: WARNING AND NOTE! Update both state_name and state_name_string
 #define MIR_RECORDER_NUM_STATES 8
-enum mir_state_name_t
-{/*{{{*/
+enum mir_state_name_t { /*{{{*/
     MIR_STATE_TIDLE = 0,
     MIR_STATE_TCREATE = 1,
     MIR_STATE_TSCHED = 2,
@@ -28,35 +27,39 @@ enum mir_state_name_t
     MIR_STATE_TPOP = 5,
     MIR_STATE_TSTEAL = 6,
     MIR_STATE_TMALLOC = 7 //Ensure this is MIR_RECORDER_NUM_STATES - 1
-};/*}}}*/
+};                        /*}}}*/
 typedef enum mir_state_name_t mir_state_name_t;
 
-struct mir_state_t
-{/*{{{*/
+struct mir_state_t { /*{{{*/
     mir_state_name_t name;
     uint64_t begin_time;
     uint64_t end_time;
     uint16_t id;
     uint64_t parent_id;
     char meta_data[MIR_RECORDER_STATE_META_DATA_MAX_SIZE];
-};/*}}}*/
+}; /*}}}*/
 
 #ifdef MIR_RECORDER_USE_HW_PERF_COUNTERS
-#ifndef __tile__ 
+#ifndef __tile__
 #define MIR_RECORDER_NUM_PAPI_HWPC 2
 #endif
 #endif
 
-#define MIR_RECORDER_STATE_BEGIN(n) if(runtime->enable_recorder == 1) mir_recorder_state_begin(n);
-#define MIR_RECORDER_STATE_END(d, l) if(runtime->enable_recorder == 1) mir_recorder_state_end(d, l);
-#define MIR_RECORDER_EVENT(d, l) if(runtime->enable_recorder == 1) mir_recorder_event(d, l);
+#define MIR_RECORDER_STATE_BEGIN(n)    \
+    if (runtime->enable_recorder == 1) \
+        mir_recorder_state_begin(n);
+#define MIR_RECORDER_STATE_END(d, l)   \
+    if (runtime->enable_recorder == 1) \
+        mir_recorder_state_end(d, l);
+#define MIR_RECORDER_EVENT(d, l)       \
+    if (runtime->enable_recorder == 1) \
+        mir_recorder_event(d, l);
 
-struct mir_event_t
-{/*{{{*/
+struct mir_event_t { /*{{{*/
     uint64_t event_time;
     uint16_t id;
 #ifdef MIR_RECORDER_USE_HW_PERF_COUNTERS
-#ifdef __tile__ 
+#ifdef __tile__
     long long values[4];
 #else
     long long values[MIR_RECORDER_NUM_PAPI_HWPC];
@@ -65,10 +68,9 @@ struct mir_event_t
     long long values[1];
 #endif
     char meta_data[MIR_RECORDER_EVENT_META_DATA_MAX_SIZE];
-};/*}}}*/
+}; /*}}}*/
 
-struct mir_recorder_t 
-{/*{{{*/
+struct mir_recorder_t { /*{{{*/
     uint64_t num_states;
     uint64_t num_events;
     uint16_t id;
@@ -97,7 +99,7 @@ struct mir_recorder_t
     // For recording events, I use a buffer
     struct mir_event_t event_buffer[MIR_RECORDER_BUFFER_MAX_SIZE];
     uint64_t event_buffer_head;
-};/*}}}*/
+}; /*}}}*/
 
 struct mir_recorder_t* mir_recorder_create(uint16_t id);
 
@@ -113,5 +115,5 @@ void mir_recorder_state_end(const char* meta_data, uint32_t meta_data_length);
 
 void mir_recorder_event(const char* meta_data, uint32_t meta_data_length);
 
-END_C_DECLS 
-#endif 
+END_C_DECLS
+#endif
