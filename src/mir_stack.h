7a434ac54197b28b44cd31eca45c310e75e07ad9
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:29:43 +0200

    Indentation and stripped white space.

--- src/mir_stack.h
+++ src/mir_stack.h
@@ -6,34 +6,32 @@
 #include <stdlib.h>
 #include "mir_types.h"
 
-BEGIN_C_DECLS 
+BEGIN_C_DECLS
 
 #ifdef MIR_STACK_DEBUG
-static void S_DBG(const char*msg, const struct mir_stack_t *q) 
-{/*{{{*/
-    fprintf(stderr, "%ld\t#%d size %d head \t%s\n", 
-                    pthread_self(),
-                    q->head - 1, q->head,
-                    msg
-                    );
-}/*}}}*/
+static void S_DBG(const char* msg, const struct mir_stack_t* q)
+{ /*{{{*/
+    fprintf(stderr, "%ld\t#%d size %d head \t%s\n",
+        pthread_self(),
+        q->head - 1, q->head,
+        msg);
+} /*}}}*/
 #else
-#define S_DBG(msg,q) 
+#define S_DBG(msg, q)
 #endif
 
 // stack state query macros
 #define STACK_FULL(stack) ((stack)->head == (stack)->capacity)
 #define STACK_EMPTY(stack) ((stack)->head == 0)
 
-struct mir_stack_t
-{/*{{{*/
+struct mir_stack_t { /*{{{*/
     void** buffer;
-    uint32_t capacity;  // Max size of stack
-    uint32_t head;      // Free location pointer, also size
+    uint32_t capacity; // Max size of stack
+    uint32_t head;     // Free location pointer, also size
 
     // Its a single-lock stack
     struct mir_lock_t lock;
-};/*}}}*/
+}; /*}}}*/
 
 // Create a stack
 struct mir_stack_t* mir_stack_create(uint32_t capacity);
