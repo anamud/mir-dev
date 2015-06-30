7bbaddbdf6fcd1407b1a73a580ac1819d2bac458
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:28:36 +0200

    Indentation and stripped white space.

--- src/mir_twc.c
+++ src/mir_twc.c
@@ -4,18 +4,18 @@
 #include "mir_runtime.h"
 
 struct mir_twc_t* mir_twc_create()
-{/*{{{*/
-    struct mir_twc_t* twc = mir_malloc_int (sizeof(struct mir_twc_t));
+{ /*{{{*/
+    struct mir_twc_t* twc = mir_malloc_int(sizeof(struct mir_twc_t));
     MIR_ASSERT(twc != NULL);
 
     // Book-keeping
-    for(int i=0; i<runtime->num_workers; i++)
-       twc->count_per_worker[i] = 0;
+    for (int i = 0; i < runtime->num_workers; i++)
+        twc->count_per_worker[i] = 0;
     twc->count = 0;
 
     // Reset num times passed
     twc->num_passes = 0;
-    twc->pass_time = (struct mir_time_list_t*) mir_malloc_int (sizeof(struct mir_time_list_t));
+    twc->pass_time = (struct mir_time_list_t*)mir_malloc_int(sizeof(struct mir_time_list_t));
     MIR_ASSERT(twc->pass_time != NULL);
     twc->pass_time->time = 0; // 0 => Not passed.
     twc->pass_time->next = NULL;
@@ -24,4 +24,4 @@ struct mir_twc_t* mir_twc_create()
     twc->parent = mir_worker_get_context()->current_task;
 
     return twc;
-}/*}}}*/
+} /*}}}*/
