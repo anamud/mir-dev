d2aff9c9922a352b44182cef1e484ba43407ae24
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:28:32 +0200

    Indentation and stripped white space.

--- src/mir_team.c
+++ src/mir_team.c
@@ -3,13 +3,13 @@
 #include "mir_utils.h"
 #include "mir_twc.h"
 
-struct mir_omp_team_t *mir_new_omp_team (struct mir_omp_team_t *pteam, unsigned nthreads)
-{/*{{{*/
-    struct mir_omp_team_t *team = mir_malloc_int(sizeof(struct mir_omp_team_t));
+struct mir_omp_team_t* mir_new_omp_team(struct mir_omp_team_t* pteam, unsigned nthreads)
+{ /*{{{*/
+    struct mir_omp_team_t* team = mir_malloc_int(sizeof(struct mir_omp_team_t));
     MIR_ASSERT(team != NULL);
     team->prev = pteam;
     team->barrier = mir_twc_create();
     team->single_count = nthreads;
     team->num_threads = nthreads;
     return team;
-}/*}}}*/
+} /*}}}*/
