56517f8de75108a231d7224f9d821a5022a70881
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:29:13 +0200

    Indentation and stripped white space.

--- src/mir_dequeue.h
+++ src/mir_dequeue.h
@@ -3,12 +3,12 @@
  * (c) The GHC Team, 2009
  *
  * Work-stealing Deque data structure
- * 
+ *
  * ---------------------------------------------------------------------------*/
 /*
 The Glasgow Haskell Compiler License
 
-Copyright 2002, The University Court of the University of Glasgow. 
+Copyright 2002, The University Court of the University of Glasgow.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -16,14 +16,14 @@ modification, are permitted provided that the following conditions are met:
 
 - Redistributions of source code must retain the above copyright notice,
 this list of conditions and the following disclaimer.
- 
+
 - Redistributions in binary form must reproduce the above copyright notice,
 this list of conditions and the following disclaimer in the documentation
 and/or other materials provided with the distribution.
- 
+
 - Neither name of the University nor the names of its contributors may be
 used to endorse or promote products derived from this software without
-specific prior written permission. 
+specific prior written permission.
 
 THIS SOFTWARE IS PROVIDED BY THE UNIVERSITY COURT OF THE UNIVERSITY OF
 GLASGOW AND THE CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
@@ -48,64 +48,64 @@ DAMAGE.
 #include "mir_memory.h"
 #include "mir_types.h"
 
-BEGIN_C_DECLS 
+BEGIN_C_DECLS
 
-#define EXTERN_INLINE static inline 
+#define EXTERN_INLINE static inline
 #define barf MIR_ABORT
 #define ASSERT MIR_ASSERT
-#define stgMallocBytes(s,m) mir_malloc_int((s))
-#define stgFree(p) mir_free_int((p),0)
+#define stgMallocBytes(s, m) mir_malloc_int((s))
+#define stgFree(p) mir_free_int((p), 0)
 typedef int StgInt;
 typedef unsigned int StgWord;
 typedef StgWord* StgPtr;
-typedef StgWord volatile*  StgVolatilePtr;   /* pointer to volatile word   */
-typedef unsigned int nat;           /* at least 32 bits (like int) */
-typedef enum { 
-    rtsFalse = 0, 
-    rtsTrue 
+typedef StgWord volatile* StgVolatilePtr; /* pointer to volatile word   */
+typedef unsigned int nat;                 /* at least 32 bits (like int) */
+typedef enum {
+    rtsFalse = 0,
+    rtsTrue
 } rtsBool;
 typedef void* StgClosurePtr;
 
 EXTERN_INLINE StgWord
 cas(StgVolatilePtr p, StgWord o, StgWord n)
-{/*{{{*/
+{ /*{{{*/
 #if i386_HOST_ARCH || x86_64_HOST_ARCH
-    __asm__ __volatile__ (
- 	  "lock\ncmpxchg %3,%1"
-          :"=a"(o), "+m" (*(volatile unsigned int *)p)
-          :"0" (o), "r" (n));
+    __asm__ __volatile__(
+        "lock\ncmpxchg %3,%1"
+        : "=a"(o), "+m"(*(volatile unsigned int*)p)
+        : "0"(o), "r"(n));
     return o;
 #elif powerpc_HOST_ARCH
     StgWord result;
-    __asm__ __volatile__ (
+    __asm__ __volatile__(
         "1:     lwarx     %0, 0, %3\n"
         "       cmpw      %0, %1\n"
         "       bne       2f\n"
         "       stwcx.    %2, 0, %3\n"
         "       bne-      1b\n"
         "2:"
-        :"=&r" (result)
-        :"r" (o), "r" (n), "r" (p)
-        :"cc", "memory"
-    );
+        : "=&r"(result)
+        : "r"(o), "r"(n), "r"(p)
+        : "cc", "memory");
     return result;
 #elif sparc_HOST_ARCH
-    __asm__ __volatile__ (
-	"cas [%1], %2, %0"
-	: "+r" (n)
-	: "r" (p), "r" (o)
-	: "memory"
-    );
+    __asm__ __volatile__(
+        "cas [%1], %2, %0"
+        : "+r"(n)
+        : "r"(p), "r"(o)
+        : "memory");
     return n;
 #elif arm_HOST_ARCH && defined(arm_HOST_ARCH_PRE_ARMv6)
     StgWord r;
     arm_atomic_spin_lock();
-    r  = *p; 
-    if (r == o) { *p = n; } 
+    r = *p;
+    if (r == o) {
+        *p = n;
+    }
     arm_atomic_spin_unlock();
     return r;
 #elif arm_HOST_ARCH && !defined(arm_HOST_ARCH_PRE_ARMv6)
-    StgWord result,tmp;
+    StgWord result, tmp;
 
     __asm__ __volatile__(
         "1:     ldrex   %1, [%2]\n"
@@ -119,9 +119,9 @@ cas(StgVolatilePtr p, StgWord o, StgWord n)
 #if !defined(arm_HOST_ARCH_PRE_ARMv7)
         "       dmb\n"
 #endif
-                : "=&r"(tmp), "=&r"(result)
-                : "r"(p), "r"(o), "r"(n)
-                : "cc","memory");
+        : "=&r"(tmp), "=&r"(result)
+        : "r"(p), "r"(o), "r"(n)
+        : "cc", "memory");
 
     return result;
 #elif !defined(WITHSMP)
@@ -136,18 +136,18 @@ cas(StgVolatilePtr p, StgWord o, StgWord n)
 #else
 #error cas() unimplemented on this architecture
 #endif
-}/*}}}*/
+} /*}}}*/
 
 EXTERN_INLINE StgWord
 atomic_inc(StgVolatilePtr p, StgWord incr)
-{/*{{{*/
+{ /*{{{*/
 #if defined(i386_HOST_ARCH) || defined(x86_64_HOST_ARCH)
     StgWord r;
     r = incr;
-    __asm__ __volatile__ (
-        "lock\nxadd %0,%1":
-            "+r" (r), "+m" (*p):
-    );
+    __asm__ __volatile__(
+        "lock\nxadd %0,%1"
+        : "+r"(r), "+m"(*p)
+        :);
     return r + incr;
 #else
     StgWord old, new;
@@ -157,19 +157,19 @@ atomic_inc(StgVolatilePtr p, StgWord incr)
     } while (cas(p, old, new) != old);
     return new;
 #endif
-}/*}}}*/
+} /*}}}*/
 
 EXTERN_INLINE StgWord
 atomic_dec(StgVolatilePtr p)
-{/*{{{*/
+{ /*{{{*/
 #if defined(i386_HOST_ARCH) || defined(x86_64_HOST_ARCH)
     StgWord r;
     r = (StgWord)-1;
-    __asm__ __volatile__ (
-        "lock\nxadd %0,%1":
-            "+r" (r), "+m" (*p):
-    );
-    return r-1;
+    __asm__ __volatile__(
+        "lock\nxadd %0,%1"
+        : "+r"(r), "+m"(*p)
+        :);
+    return r - 1;
 #else
     StgWord old, new;
     do {
@@ -178,18 +178,18 @@ atomic_dec(StgVolatilePtr p)
     } while (cas(p, old, new) != old);
     return new;
 #endif
-}/*}}}*/
+} /*}}}*/
 
 EXTERN_INLINE void
 busy_wait_nop(void)
-{/*{{{*/
+{ /*{{{*/
 #if defined(i386_HOST_ARCH) || defined(x86_64_HOST_ARCH)
-    __asm__ __volatile__ ("rep; nop");
-    //
+    __asm__ __volatile__("rep; nop");
+//
 #else
-    // nothing
+// nothing
 #endif
-}/*}}}*/
+} /*}}}*/
 
 /*
  * We need to tell both the compiler AND the CPU about the barriers.
@@ -198,19 +198,34 @@ busy_wait_nop(void)
  * on each of the barriers below.
  */
 EXTERN_INLINE void
-write_barrier(void) 
-{/*{{{*/
+write_barrier(void)
+{ /*{{{*/
 #if i386_HOST_ARCH || x86_64_HOST_ARCH
-    __asm__ __volatile__ ("" : : : "memory");
+    __asm__ __volatile__(""
+                         :
+                         :
+                         : "memory");
 #elif powerpc_HOST_ARCH
-    __asm__ __volatile__ ("lwsync" : : : "memory");
+    __asm__ __volatile__("lwsync"
+                         :
+                         :
+                         : "memory");
 #elif sparc_HOST_ARCH
     /* Sparc in TSO mode does not require store/store barriers. */
-    __asm__ __volatile__ ("" : : : "memory");
+    __asm__ __volatile__(""
+                         :
+                         :
+                         : "memory");
 #elif arm_HOST_ARCH && defined(arm_HOST_ARCH_PRE_ARMv7)
-    __asm__ __volatile__ ("" : : : "memory");
+    __asm__ __volatile__(""
+                         :
+                         :
+                         : "memory");
 #elif arm_HOST_ARCH && !defined(arm_HOST_ARCH_PRE_ARMv7)
-    __asm__ __volatile__ ("dmb  st" : : : "memory");
+    __asm__ __volatile__("dmb  st"
+                         :
+                         :
+                         : "memory");
 #elif !defined(WITHSMP)
     return;
 #elif defined(__tile__)
@@ -218,21 +233,36 @@ write_barrier(void)
 #else
 #error memory barriers unimplemented on this architecture
 #endif
-}/*}}}*/
+} /*}}}*/
 
 EXTERN_INLINE void
-store_load_barrier(void) 
-{/*{{{*/
+store_load_barrier(void)
+{ /*{{{*/
 #if i386_HOST_ARCH
-    __asm__ __volatile__ ("lock; addl $0,0(%%esp)" : : : "memory");
+    __asm__ __volatile__("lock; addl $0,0(%%esp)"
+                         :
+                         :
+                         : "memory");
 #elif x86_64_HOST_ARCH
-    __asm__ __volatile__ ("lock; addq $0,0(%%rsp)" : : : "memory");
+    __asm__ __volatile__("lock; addq $0,0(%%rsp)"
+                         :
+                         :
+                         : "memory");
 #elif powerpc_HOST_ARCH
-    __asm__ __volatile__ ("sync" : : : "memory");
+    __asm__ __volatile__("sync"
+                         :
+                         :
+                         : "memory");
 #elif sparc_HOST_ARCH
-    __asm__ __volatile__ ("membar #StoreLoad" : : : "memory");
+    __asm__ __volatile__("membar #StoreLoad"
+                         :
+                         :
+                         : "memory");
 #elif arm_HOST_ARCH && !defined(arm_HOST_ARCH_PRE_ARMv7)
-    __asm__ __volatile__ ("dmb" : : : "memory");
+    __asm__ __volatile__("dmb"
+                         :
+                         :
+                         : "memory");
 #elif !defined(WITHSMP)
     return;
 #elif defined(__tile__)
@@ -240,22 +270,37 @@ store_load_barrier(void)
 #else
 #error memory barriers unimplemented on this architecture
 #endif
-}/*}}}*/
+} /*}}}*/
 
 EXTERN_INLINE void
-load_load_barrier(void) 
-{/*{{{*/
+load_load_barrier(void)
+{ /*{{{*/
 #if i386_HOST_ARCH
-    __asm__ __volatile__ ("" : : : "memory");
+    __asm__ __volatile__(""
+                         :
+                         :
+                         : "memory");
 #elif x86_64_HOST_ARCH
-    __asm__ __volatile__ ("" : : : "memory");
+    __asm__ __volatile__(""
+                         :
+                         :
+                         : "memory");
 #elif powerpc_HOST_ARCH
-    __asm__ __volatile__ ("lwsync" : : : "memory");
+    __asm__ __volatile__("lwsync"
+                         :
+                         :
+                         : "memory");
 #elif sparc_HOST_ARCH
     /* Sparc in TSO mode does not require load/load barriers. */
-    __asm__ __volatile__ ("" : : : "memory");
+    __asm__ __volatile__(""
+                         :
+                         :
+                         : "memory");
 #elif arm_HOST_ARCH && !defined(arm_HOST_ARCH_PRE_ARMv7)
-    __asm__ __volatile__ ("dmb" : : : "memory");
+    __asm__ __volatile__("dmb"
+                         :
+                         :
+                         : "memory");
 #elif !defined(WITHSMP)
     return;
 #elif defined(__tile__)
@@ -263,7 +308,7 @@ load_load_barrier(void)
 #else
 #error memory barriers unimplemented on this architecture
 #endif
-}/*}}}*/
+} /*}}}*/
 
 // Load a pointer from a memory location that might be being modified
 // concurrently.  This prevents the compiler from optimising away
@@ -273,8 +318,8 @@ load_load_barrier(void)
 
 typedef struct WSDeque_ {
     // Size of elements array. Used for modulo calculation: we round up
-    // to powers of 2 and use the dyadic log (modulo == bitwise &) 
-    StgWord size; 
+    // to powers of 2 and use the dyadic log (modulo == bitwise &)
+    StgWord size;
     StgWord moduloSize; /* bitmask for modulo */
 
     // top, index where multiple readers steal() (protected by a cas)
@@ -286,14 +331,14 @@ typedef struct WSDeque_ {
 
     // both top and bottom are continuously incremented, and used as
     // an index modulo the current array size.
-  
+
     // lower bound on the current top value. This is an internal
     // optimisation to avoid unnecessarily accessing the top field
     // inside pushBottom
     volatile StgWord topBound;
 
     // The elements array
-    void ** elements;
+    void** elements;
 
     //  Please note: the dataspace cannot follow the admin fields
     //  immediately, as it should be possible to enlarge it without
@@ -304,20 +349,20 @@ typedef WSDeque mir_dequeue_t;
 
 /* INVARIANTS, in this order: reasonable size,
    topBound consistent, space pointer, space accessible to us.
-   
+
    NB. This is safe to use only (a) on a spark pool owned by the
    current thread, or (b) when there's only one thread running, or no
    stealing going on (e.g. during GC).
 */
-#define ASSERT_WSDEQUE_INVARIANTS(p)         \
-  ASSERT((p)->size > 0);                        \
-  ASSERT((p)->topBound <= (p)->top);            \
-  ASSERT((p)->elements != NULL);                \
-  ASSERT(*((p)->elements) || 1);                \
-  ASSERT(*((p)->elements - 1  + ((p)->size)) || 1);
+#define ASSERT_WSDEQUE_INVARIANTS(p)   \
+    ASSERT((p)->size > 0);             \
+    ASSERT((p)->topBound <= (p)->top); \
+    ASSERT((p)->elements != NULL);     \
+    ASSERT(*((p)->elements) || 1);     \
+    ASSERT(*((p)->elements - 1 + ((p)->size)) || 1);
 
 // No: it is possible that top > bottom when using pop()
-//  MIR_ASSERT((p)->bottom >= (p)->top);           
+//  MIR_ASSERT((p)->bottom >= (p)->top);
 //  MIR_ASSERT((p)->size > (p)->bottom - (p)->top);
 
 /* -----------------------------------------------------------------------------
@@ -330,61 +375,61 @@ typedef WSDeque mir_dequeue_t;
  * -------------------------------------------------------------------------- */
 
 // Allocation, deallocation
-WSDeque * newWSDeque  (nat size);
-void      freeWSDeque (WSDeque *q);
+WSDeque* newWSDeque(nat size);
+void freeWSDeque(WSDeque* q);
 
 // Take an element from the "write" end of the pool.  Can be called
 // by the pool owner only.
-void* popWSDeque (WSDeque *q);
+void* popWSDeque(WSDeque* q);
 
 // Push onto the "write" end of the pool.  Return true if the push
 // succeeded, or false if the deque is full.
-rtsBool pushWSDeque (WSDeque *q, void *elem);
+rtsBool pushWSDeque(WSDeque* q, void* elem);
 
 // Removes all elements from the deque
-EXTERN_INLINE void discardElements (WSDeque *q);
+EXTERN_INLINE void discardElements(WSDeque* q);
 
 // Removes an element of the deque from the "read" end, or returns
 // NULL if the pool is empty, or if there was a collision with another
 // thief.
-void * stealWSDeque_ (WSDeque *q);
+void* stealWSDeque_(WSDeque* q);
 
 // Removes an element of the deque from the "read" end, or returns
 // NULL if the pool is empty.
-void * stealWSDeque (WSDeque *q);
+void* stealWSDeque(WSDeque* q);
 
 // "guesses" whether a deque is empty. Can return false negatives in
 //  presence of concurrent steal() calls, and false positives in
 //  presence of a concurrent pushBottom().
-EXTERN_INLINE rtsBool looksEmptyWSDeque (WSDeque* q);
+EXTERN_INLINE rtsBool looksEmptyWSDeque(WSDeque* q);
 
-EXTERN_INLINE long dequeElements   (WSDeque *q);
+EXTERN_INLINE long dequeElements(WSDeque* q);
 
 /* -----------------------------------------------------------------------------
  * PRIVATE below here
  * -------------------------------------------------------------------------- */
 
 EXTERN_INLINE long
-dequeElements (WSDeque *q)
-{/*{{{*/
+dequeElements(WSDeque* q)
+{ /*{{{*/
     StgWord t = q->top;
     StgWord b = q->bottom;
     // try to prefer false negatives by reading top first
     return ((long)b - (long)t);
-}/*}}}*/
+} /*}}}*/
 
 EXTERN_INLINE rtsBool
-looksEmptyWSDeque (WSDeque *q)
-{/*{{{*/
+looksEmptyWSDeque(WSDeque* q)
+{ /*{{{*/
     return (dequeElements(q) <= 0);
-}/*}}}*/
+} /*}}}*/
 
 EXTERN_INLINE void
-discardElements (WSDeque *q)
-{/*{{{*/
+discardElements(WSDeque* q)
+{ /*{{{*/
     q->top = q->bottom;
-//    pool->topBound = pool->top;
-}/*}}}*/
+    //    pool->topBound = pool->top;
+} /*}}}*/
 
-END_C_DECLS 
-#endif // MIR_DEQUEUE_H 
+END_C_DECLS
+#endif // MIR_DEQUEUE_H
