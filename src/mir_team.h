de4fa35596166210a0afc16f8a30bbd2f1dd0ae9
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:29:47 +0200

    Indentation and stripped white space.

--- src/mir_team.h
+++ src/mir_team.h
@@ -1,20 +1,18 @@
 #ifndef MIR_TEAM_H
 #define MIR_TEAM_H 1
 
-BEGIN_C_DECLS 
+BEGIN_C_DECLS
 
 /*LIBINT_BASE_DECL_BEGIN*/
-struct mir_omp_team_t
-{/*{{{*/
-    struct mir_omp_team_t *prev;
+struct mir_omp_team_t { /*{{{*/
+    struct mir_omp_team_t* prev;
     struct mir_twc_t* barrier;
     int num_threads;
     int single_count;
-};/*}}}*/
-/*LIBINT_BASE_DECL_END*/
+}; /*}}}*/
+   /*LIBINT_BASE_DECL_END*/
 
-
-/*LIBINT*/ struct mir_omp_team_t *mir_new_omp_team (struct mir_omp_team_t *, unsigned);
+/*LIBINT*/ struct mir_omp_team_t* mir_new_omp_team(struct mir_omp_team_t*, unsigned);
 
 END_C_DECLS
 #endif
