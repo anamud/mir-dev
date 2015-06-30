2483ceefa69dc07fedd2e2b2f8d5f294167f738c
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:29:36 +0200

    Indentation and stripped white space.

--- src/mir_queue.h
+++ src/mir_queue.h
@@ -6,37 +6,35 @@
 #include <stdlib.h>
 #include "mir_types.h"
 
-BEGIN_C_DECLS 
+BEGIN_C_DECLS
 
 #ifdef MIR_QUEUE_DEBUG
-static void Q_DBG(const char*msg, const struct mir_queue_t *q) 
-{/*{{{*/
-    fprintf(stderr, "%ld\t#%d in %d out %d\t%s\n", 
-                    pthread_self(),
-                    q->size, q->in, q->out,
-                    msg
-                    );
-}/*}}}*/
+static void Q_DBG(const char* msg, const struct mir_queue_t* q)
+{ /*{{{*/
+    fprintf(stderr, "%ld\t#%d in %d out %d\t%s\n",
+        pthread_self(),
+        q->size, q->in, q->out,
+        msg);
+} /*}}}*/
 #else
-#define Q_DBG(msg,q) 
+#define Q_DBG(msg, q)
 #endif
 
 // Queue state query macros
 #define QUEUE_FULL(queue) ((queue)->size == (queue)->capacity)
 #define QUEUE_EMPTY(queue) ((queue)->size == 0)
 
-struct mir_queue_t
-{/*{{{*/
+struct mir_queue_t { /*{{{*/
     void** buffer;
-    uint32_t capacity;  // Max size of queue
-    uint32_t size;      // Number of elements
-    uint32_t in;        // Heads
+    uint32_t capacity; // Max size of queue
+    uint32_t size;     // Number of elements
+    uint32_t in;       // Heads
     uint32_t out;
 
     // Its a two-lock queue
     struct mir_lock_t enq_lock;
     struct mir_lock_t deq_lock;
-};/*}}}*/
+}; /*}}}*/
 
 // Create a queue
 struct mir_queue_t* mir_queue_create(uint32_t capacity);
@@ -53,6 +51,6 @@ void mir_queue_pop(struct mir_queue_t* queue, void** data);
 // Get queue size
 uint32_t mir_queue_size(const struct mir_queue_t* queue);
 
-END_C_DECLS 
+END_C_DECLS
 
 #endif
