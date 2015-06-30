e708d42f59061e8e1bc31847bcdf63912f219349
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:29:34 +0200

    Indentation and stripped white space.

--- src/mir_omp_int.h
+++ src/mir_omp_int.h
@@ -5,57 +5,56 @@
 
 #include "mir_types.h"
 
-BEGIN_C_DECLS 
+BEGIN_C_DECLS
 
-enum omp_for_schedule_t
-{
-  OFS_RUNTIME,
-  OFS_STATIC,
-  OFS_DYNAMIC,
-  OFS_GUIDED,
-  OFS_AUTO
+enum omp_for_schedule_t {
+    OFS_RUNTIME,
+    OFS_STATIC,
+    OFS_DYNAMIC,
+    OFS_GUIDED,
+    OFS_AUTO
 };
 
 /* Refactored from GCC git repository git://gcc.gnu.org/git/gcc.git HEAD ae76874abdf11bb77597f7285cb115bd78e82fda */
-// FIXME: Warning!! Fragile!! Potential version mismatch when linked with different libgomp on system. Add a check! 
+// FIXME: Warning!! Fragile!! Potential version mismatch when linked with different libgomp on system. Add a check!
 
 /* env.c */
-void GOMP_parse_schedule (void);
+void GOMP_parse_schedule(void);
 
 /* barrier.c */
 
-void GOMP_barrier (void);
+void GOMP_barrier(void);
 
 /* critical.c */
 
-void GOMP_critical_start (void);
-void GOMP_critical_end (void);
+void GOMP_critical_start(void);
+void GOMP_critical_end(void);
 
 /* loop.c */
 
-bool GOMP_loop_dynamic_next (long *istart, long *iend);
-void GOMP_parallel_loop_dynamic (void (*fn) (void *), void *data, unsigned num_threads, long start, long end, long incr, long chunk_size, unsigned flags);
-bool GOMP_loop_static_next (long *istart, long *iend);
-void GOMP_parallel_loop_static (void (*fn) (void *), void *data, unsigned num_threads, long start, long end, long incr, long chunk_size, unsigned flags);
-bool GOMP_loop_runtime_next (long *istart, long *iend);
-void GOMP_parallel_loop_runtime (void (*fn) (void *), void *data, unsigned num_threads, long start, long end, long incr, unsigned flags);
-void GOMP_loop_end (void);
-void GOMP_loop_end_nowait (void);
+bool GOMP_loop_dynamic_next(long* istart, long* iend);
+void GOMP_parallel_loop_dynamic(void (*fn)(void*), void* data, unsigned num_threads, long start, long end, long incr, long chunk_size, unsigned flags);
+bool GOMP_loop_static_next(long* istart, long* iend);
+void GOMP_parallel_loop_static(void (*fn)(void*), void* data, unsigned num_threads, long start, long end, long incr, long chunk_size, unsigned flags);
+bool GOMP_loop_runtime_next(long* istart, long* iend);
+void GOMP_parallel_loop_runtime(void (*fn)(void*), void* data, unsigned num_threads, long start, long end, long incr, unsigned flags);
+void GOMP_loop_end(void);
+void GOMP_loop_end_nowait(void);
 
 /* parallel.c */
 
-void GOMP_parallel_start (void (*fn) (void *), void *data, unsigned num_threads);
-void GOMP_parallel (void (*fn) (void *), void *data, unsigned num_threads, unsigned int flags);
-void GOMP_parallel_end (void);
+void GOMP_parallel_start(void (*fn)(void*), void* data, unsigned num_threads);
+void GOMP_parallel(void (*fn)(void*), void* data, unsigned num_threads, unsigned int flags);
+void GOMP_parallel_end(void);
 
 /* task.c */
 
-void GOMP_task (void (*fn) (void *), void *data, void (*cpyfn) (void *, void *), long arg_size, long arg_align, bool if_clause, unsigned flags, void **depend);
-void GOMP_taskwait (void);
+void GOMP_task(void (*fn)(void*), void* data, void (*cpyfn)(void*, void*), long arg_size, long arg_align, bool if_clause, unsigned flags, void** depend);
+void GOMP_taskwait(void);
 
 /* single.c */
 
-bool GOMP_single_start (void);
+bool GOMP_single_start(void);
 
-END_C_DECLS 
+END_C_DECLS
 #endif
