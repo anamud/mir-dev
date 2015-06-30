e3ab298c5e4b92cfbae06b6144e8840d426f53d3
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:29:29 +0200

    Indentation and stripped white space.

--- src/mir_memory.h
+++ src/mir_memory.h
@@ -1,5 +1,5 @@
-#ifndef  MIR_MEMORY_H
-#define  MIR_MEMORY_H 1
+#ifndef MIR_MEMORY_H
+#define MIR_MEMORY_H 1
 
 #ifdef __tile__
 #include <tmc/alloc.h>
@@ -9,31 +9,31 @@
 #include <stdint.h>
 #include "mir_types.h"
 
-BEGIN_C_DECLS 
+BEGIN_C_DECLS
 
 void* mir_malloc_int(size_t bytes);
 
 void* mir_cmalloc_int(size_t bytes);
 
-void mir_free_int(void *p, size_t bytes);
+void mir_free_int(void* p, size_t bytes);
 
 uint64_t mir_get_allocated_memory();
 
 #ifdef __tile__
 static inline void page_attr_set(tmc_alloc_t* alloc)
-{/*{{{*/
-#if defined (MIR_PAGE_NO_LOCAL_CACHING)
+{ /*{{{*/
+#if defined(MIR_PAGE_NO_LOCAL_CACHING)
     tmc_alloc_set_caching(alloc, MAP_CACHE_NO_LOCAL);
-#elif defined (MIR_PAGE_NO_L1_CACHING)
+#elif defined(MIR_PAGE_NO_L1_CACHING)
     tmc_alloc_set_caching(alloc, MAP_CACHE_NO_L1);
-#elif defined (MIR_PAGE_NO_L2_CACHING)
+#elif defined(MIR_PAGE_NO_L2_CACHING)
     tmc_alloc_set_caching(alloc, MAP_CACHE_NO_L2);
 #endif
-return;
-}/*}}}*/
+    return;
+} /*}}}*/
 #endif
 
-END_C_DECLS 
+END_C_DECLS
 
-#endif  
+#endif
 
