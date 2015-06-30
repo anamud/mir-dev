04d5b627824139f31b0b8f0e3e5fbc3fd39fb020
Ananya Muddukrishna <ananya@kth.se>
2015-06-30 17:29:31 +0200

    Indentation and stripped white space.

--- src/mir_mem_pol.h
+++ src/mir_mem_pol.h
@@ -5,44 +5,42 @@
 #include "mir_types.h"
 #include "mir_defines.h"
 
-BEGIN_C_DECLS 
+BEGIN_C_DECLS
 #ifdef MIR_MEM_POL_ENABLE
 
 // Memory allocation related
 
-void mir_mem_pol_config (const char* conf_str);
+void mir_mem_pol_config(const char* conf_str);
 
-void mir_mem_pol_create ();
+void mir_mem_pol_create();
 
-void mir_mem_pol_init ();
+void mir_mem_pol_init();
 
-void mir_mem_pol_destroy ();
+void mir_mem_pol_destroy();
 
-/*LIBINT*/ void mir_mem_pol_reset ();
+/*LIBINT*/ void mir_mem_pol_reset();
 
-/*LIBINT*/ void* mir_mem_pol_allocate (size_t sz);
+/*LIBINT*/ void* mir_mem_pol_allocate(size_t sz);
 
-/*LIBINT*/ void mir_mem_pol_release (void* addr, size_t sz);
+/*LIBINT*/ void mir_mem_pol_release(void* addr, size_t sz);
 
 // Memory node distribution related
 
-struct mir_mem_node_dist_t
-{/*{{{*/
+struct mir_mem_node_dist_t { /*{{{*/
     size_t* buf;
-};/*}}}*/
+}; /*}}}*/
 
-struct mir_mem_node_dist_stat_t
-{/*{{{*/
+struct mir_mem_node_dist_stat_t { /*{{{*/
     size_t sum;
     size_t min;
     size_t max;
     double mean;
     double sd;
-};/*}}}*/
+}; /*}}}*/
 
 struct mir_mem_node_dist_t* mir_mem_node_dist_create();
 
-void  mir_mem_node_dist_destroy(struct mir_mem_node_dist_t* dist);
+void mir_mem_node_dist_destroy(struct mir_mem_node_dist_t* dist);
 
 void mir_mem_get_mem_node_dist(struct mir_mem_node_dist_t* dist, void* addr, size_t sz, void* part_of);
 
