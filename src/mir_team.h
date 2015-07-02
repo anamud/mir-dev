2114c8ead483d043f43d6bd8906eb49d32835d08
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2015-07-02 15:37:25 +0200

    Hide mir_omp_team_t implementation

--- src/mir_team.h
+++ src/mir_team.h
@@ -4,13 +4,15 @@
 BEGIN_C_DECLS
 
 /*LIBINT_BASE_DECL_BEGIN*/
+typedef struct mir_omp_team_t mir_omp_team_t;
+/*LIBINT_BASE_DECL_END*/
+
 struct mir_omp_team_t { /*{{{*/
     struct mir_omp_team_t* prev;
     struct mir_twc_t* barrier;
     int num_threads;
     int single_count;
 }; /*}}}*/
-   /*LIBINT_BASE_DECL_END*/
 
 /*LIBINT*/ struct mir_omp_team_t* mir_new_omp_team(struct mir_omp_team_t*, unsigned);
 
