d141a31101c31fb44b994d0a1f4dce4fe49050aa
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2015-07-30 17:28:17 +0200

    Continue if the queue looks empty. No functional changes.

--- src/scheduling/mir_sched_pol_ws_de.c
+++ src/scheduling/mir_sched_pol_ws_de.c
@@ -102,33 +102,34 @@ int pop_ws_de(struct mir_task_t** task)
         }
 
         mir_dequeue_t* queue = (mir_dequeue_t*)sp->queues[ctr];
-        if (looksEmptyWSDeque(queue) == rtsFalse) {
-            *task = ctr == worker->id ? (struct mir_task_t*)popWSDeque(queue) : (struct mir_task_t*)stealWSDeque(queue);
-            if (*task) {
-                bool grab = __sync_bool_compare_and_swap(&((*task)->taken), 0, 1);
-                if (grab) {
-                    // Update stats
-                    if (runtime->enable_worker_stats == 1) {
+        if (looksEmptyWSDeque(queue) == rtsTrue)
+            continue;
+
+        *task = ctr == worker->id ? (struct mir_task_t*)popWSDeque(queue) : (struct mir_task_t*)stealWSDeque(queue);
+        if (*task) {
+            bool grab = __sync_bool_compare_and_swap(&((*task)->taken), 0, 1);
+            if (grab) {
+                // Update stats
+                if (runtime->enable_worker_stats == 1) {
 #ifdef MIR_MEM_POL_ENABLE
-                        uint16_t node = runtime->arch->node_of(worker->cpu_id);
-                        struct mir_mem_node_dist_t* dist = mir_task_get_mem_node_dist(*task, MIR_DATA_ACCESS_READ);
-                        if (dist) {
-                            (*task)->comm_cost = mir_mem_node_dist_get_comm_cost(dist, node);
-                            mir_worker_statistics_update_comm_cost(worker->statistics, (*task)->comm_cost);
-                        }
-#endif
-                        if (ctr == worker->id)
-                            worker->statistics->num_tasks_owned++;
-                        else
-                            worker->statistics->num_tasks_stolen++;
+                    uint16_t node = runtime->arch->node_of(worker->cpu_id);
+                    struct mir_mem_node_dist_t* dist = mir_task_get_mem_node_dist(*task, MIR_DATA_ACCESS_READ);
+                    if (dist) {
+                        (*task)->comm_cost = mir_mem_node_dist_get_comm_cost(dist, node);
+                        mir_worker_statistics_update_comm_cost(worker->statistics, (*task)->comm_cost);
                     }
-
-                    __sync_fetch_and_sub(&g_num_tasks_waiting, 1);
-                    MIR_ASSERT(g_num_tasks_waiting >= 0);
-                    T_DBG(ctr == worker->id ? : "Dq" : "St", *task);
-
-                    return 1;
+#endif
+                    if (ctr == worker->id)
+                        worker->statistics->num_tasks_owned++;
+                    else
+                        worker->statistics->num_tasks_stolen++;
                 }
+
+                __sync_fetch_and_sub(&g_num_tasks_waiting, 1);
+                MIR_ASSERT(g_num_tasks_waiting >= 0);
+                T_DBG(ctr == worker->id ? : "Dq" : "St", *task);
+
+                return 1;
             }
         }
     } while (++ctr != worker->id);
