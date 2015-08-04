f656b4a562059c35d28750cf747728b9ef80bdca
Ananya Muddukrishna <ananya@kth.se>
2015-08-04 15:09:57 +0200

    Fix indentation and whitespace.

--- scripts/profiling/task/compare-task-stats.R
+++ scripts/profiling/task/compare-task-stats.R
@@ -11,33 +11,35 @@ comp.type.default.file <- "scripts/profiling/task/task-stats-comparison-default.
 
 # Read arguments
 option_list <- list(
-make_option(c("-l","--left"), help = "Task stats left.", metavar="FILE"),
-make_option(c("-r","--right"), help = "Task stats right.", metavar="FILE"),
-make_option(c("-k","--key"), default="lineage", help = "Column used for comparison [default \"%default\"]", metavar="STRING"),
-make_option(c("--config"), default=comp.type.default.file, help = "Comparison configuration [default \"%default\"]", metavar="FILE"),
-make_option(c("-o","--out"), default="task-stats", help = "Output file suffix [default \"%default\"].", metavar="STRING"),
-make_option(c("--verbose"), action="store_true", default=TRUE, help="Print output [default]."),
-make_option(c("--timing"), action="store_true", default=FALSE, help="Print timing information."),
-make_option(c("--quiet"), action="store_false", dest="verbose", help="Print little output."))
+                    make_option(c("-l","--left"), help = "Task stats left.", metavar="FILE"),
+                    make_option(c("-r","--right"), help = "Task stats right.", metavar="FILE"),
+                    make_option(c("-k","--key"), default="lineage", help = "Column used for comparison [default \"%default\"]", metavar="STRING"),
+                    make_option(c("--config"), default=comp.type.default.file, help = "Comparison configuration [default \"%default\"]", metavar="FILE"),
+                    make_option(c("-o","--out"), default="task-stats", help = "Output file suffix [default \"%default\"].", metavar="STRING"),
+                    make_option(c("--verbose"), action="store_true", default=TRUE, help="Print output [default]."),
+                    make_option(c("--timing"), action="store_true", default=FALSE, help="Print timing information."),
+                    make_option(c("--quiet"), action="store_false", dest="verbose", help="Print little output."))
+
 parsed <- parse_args(OptionParser(option_list = option_list), args = commandArgs(TRUE))
-if(!exists("left", where=parsed) | !exists("right", where=parsed))
-{
+
+if(!exists("left", where=parsed) | !exists("right", where=parsed)) {
     print("Error: Invalid arguments. Check help (-h)")
     quit("no", 1)
 }
 
 # Read data
 if(parsed$verbo) print("Reading task stats ...")
+
 ts.data.l <- read.csv(parsed$left, header=TRUE)
 ts.data.r <- read.csv(parsed$right, header=TRUE)
-if(!(parsed$key %in% colnames(ts.data.l)) | !(parsed$key %in% colnames(ts.data.r)))
-{
+
+if(!(parsed$key %in% colnames(ts.data.l)) | !(parsed$key %in% colnames(ts.data.r))) {
     print("Error: Key not found in task stats. Aborting!")
     quit("no", 1)
 }
+
 if(parsed$verbo) print("Reading comparison configuration ...")
-if(parsed$config == comp.type.default.file)
-{
+if(parsed$config == comp.type.default.file) {
     comp.type <- read.csv(paste(mir_root, comp.type.default.file, sep="/"), header=T)
 } else {
     comp.type <- read.csv(parsed$config, header=T)
@@ -49,15 +51,13 @@ ts.data.out <- subset(ts.data.l, select=parsed$key)
 # Compare
 if(parsed$timing) tic(type="elapsed")
 if(parsed$verbose) print("Comparing task stats ...")
-for(r in seq(1,nrow(comp.type)))
-{
+for(r in seq(1,nrow(comp.type))) {
     # Paramters
     attrib <- as.character(comp.type[r,]$comp.attrib)
     op <- as.character(comp.type[r,]$comp.op)
     name <- as.character(comp.type[r,]$comp.name)
 
-    if(attrib %in% colnames(ts.data.l) & attrib %in% colnames(ts.data.l))
-    {
+    if(attrib %in% colnames(ts.data.l) & attrib %in% colnames(ts.data.l)) {
         if(parsed$verbose) print(paste("Processing comparison type:" , attrib, op, name))
 
         # Subset and merge left and right task stats
