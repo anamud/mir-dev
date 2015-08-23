70755e895c51b3c02c037a3f3985f03f7135201c
Ananya Muddukrishna <ananya@kth.se>
2015-08-23 22:20:21 +0200

    Script to extract loop schedule from tast stats.

---
+++ scripts/profiling/task/for-loop/dump-loop-schedule.R
@@ -0,0 +1,63 @@
+# Clear workspace
+rm(list=ls())
+
+# Strings as factors
+options(stringsAsFactors = F)
+
+# Import
+mir_root <- Sys.getenv("MIR_ROOT")
+source(paste(mir_root,"/scripts/profiling/task/common.R",sep=""))
+
+# Libary
+library(optparse, quietly=TRUE)
+suppressMessages(library(dplyr))
+
+# Read arguments
+Rstudio_mode <- F
+if (Rstudio_mode) {
+    parsed <- list(data="loop-task-stats.processed",
+                   verbose=T,
+                   timing=F)
+} else {
+    option_list <- list(
+                        make_option(c("-d","--data"), help = "Processed task stats.", metavar="FILE"),
+                        make_option(c("--verbose"), action="store_true", default=TRUE, help="Print output [default]."),
+                        make_option(c("--timing"), action="store_true", default=FALSE, help="Print timing information."),
+                        make_option(c("--quiet"), action="store_false", dest="verbose", help="Print little output."))
+
+    parsed <- parse_args(OptionParser(option_list = option_list), args = commandArgs(TRUE))
+
+    if (!exists("data", where=parsed)) {
+        my_print("Error: Invalid arguments. Check help (-h).")
+        quit("no", 1)
+    }
+}
+
+# Read data
+if (parsed$verbose) my_print(paste("Reading file", parsed$data))
+d <- read.csv(parsed$data, header=TRUE)
+
+# Filter and group by outline function and idle join
+e <- d %>% filter(!is.na(metadata) & grepl(glob2rx("chunk_*_*"), metadata)) %>% group_by(outline_function,idle_join)
+e1 <- e %>% summarize(count = n(), chunk_work_cpu_balance = median(chunk_work_cpu_balance))
+
+# Print each group to file
+for (i in seq(1:nrow(e1)))
+{
+    of <- e1[i,]$outline_function
+    ij <- e1[i,]$idle_join
+
+    f <- e %>% filter(outline_function == of & idle_join == ij)
+    # Convert work cycles to time assuming frequency is 800M.
+    # The conversion is necessary to keep constraint search within integer bounds.
+    g <- f %>% rowwise() %>% mutate(work_time_us = ceiling(work_cycles * (1/800)), chunk_start = as.numeric(unlist(strsplit(metadata, "_"))[2]), chunk_end = as.numeric(unlist(strsplit(metadata, "_"))[3]))
+
+    out_file <- paste(paste("loop", of, ij, sep="_"), "schedule", sep=".")
+    write.csv(select(g, chunk_start, chunk_end, cpu_id, work_time_us), file=out_file, row.names=F)
+    print(paste("Wrote file:", out_file))
+}
+
+# Print warnings
+wa <- warnings()
+if (class(wa) != "NULL")
+    print(wa)
