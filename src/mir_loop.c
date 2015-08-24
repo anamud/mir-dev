f7e9900954578bc6a47e02395b33aebac33026f8
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2015-08-24 13:17:46 +0200

    Invert logic and remove one more layer of indentation.

--- src/mir_loop.c
+++ src/mir_loop.c
@@ -28,75 +28,74 @@ void mir_omp_loop_desc_init(struct mir_loop_des_t* loop, long start, long end,
     loop->static_trip = 0;
     loop->non_parallel_start = 0;
 
-    if (use_precomp_schedule) {
-        struct mir_worker_t* worker = mir_worker_get_context();
-        MIR_ASSERT(worker != NULL);
-        MIR_ASSERT(worker->current_task != NULL);
+    if (!use_precomp_schedule) {
+        goto no_percomputed_schedule;
+    }
 
-        if (worker->current_task->parent) {
-            unsigned long idle_join = (strcmp(worker->current_task->parent->name, "idle_task") == 0) ? worker->current_task->twc->num_passes : worker->current_task->parent->twc->num_passes;
+    struct mir_worker_t* worker = mir_worker_get_context();
+    MIR_ASSERT(worker != NULL);
+    MIR_ASSERT(worker->current_task != NULL);
 
-            char schedule_file_name[MIR_LONG_NAME_LEN + MIR_SHORT_NAME_LEN];
-            char task_of[MIR_SHORT_NAME_LEN];
-            sprintf(task_of, "%p", worker->current_task->func);
-            sprintf(schedule_file_name, "%s/loop_%lu_%lu.schedule_opt",
-                    runtime->precomp_schedule_dir,
-                    strtol(task_of, NULL, 16),
-                    idle_join);
+    if (worker->current_task->parent) {
+        unsigned long idle_join = (strcmp(worker->current_task->parent->name, "idle_task") == 0) ? worker->current_task->twc->num_passes : worker->current_task->parent->twc->num_passes;
 
-            FILE* fp = fopen(schedule_file_name, "r");
-            if (fp == NULL) {
-                goto no_percomputed_schedule;
-            }
-            int num_expect = 4;
-            int retval;
-            if (worker->id == 0) {
-                MIR_LOG_INFO("Using precomputed schedule file: %s.",
-                        schedule_file_name);
-            }
-            unsigned long chunk_start, chunk_end, cpu_id, work_cycles;
-            while (!feof(fp)) {
-                while (num_expect == (retval = fscanf(fp,
-                                          "%lu,%lu,%lu,%lu\n",
-                                          &chunk_start,
-                                          &chunk_end,
-                                          &cpu_id,
-                                          &work_cycles))) {
-                    loop->precomp_schedule_exists = true;
-                    if (cpu_id == MIR_IMPOSSIBLE_CPU_ID) {
-                        MIR_LOG_ERR("Precomputed schedule in file %s uses unsupported MIR_IMPOSSIBLE_CPU_ID.",
-                                    schedule_file_name);
-                    }
-                    else if (cpu_id > runtime->num_workers) {
-                        MIR_LOG_ERR("Precomputed schedule in file %s has more workers than available.",
-                                    schedule_file_name);
-                    }
-                    if (cpu_id == worker->cpu_id) {
-                        struct mir_loop_schedule_t* schedule = mir_malloc_int(sizeof(struct mir_loop_schedule_t));
-                        MIR_CHECK_MEM(schedule != NULL);
-                        schedule->chunk_start = chunk_start;
-                        schedule->chunk_end = chunk_end;
-                        schedule->cpu_id = cpu_id;
-                        schedule->work_cycles = work_cycles;
+        char schedule_file_name[MIR_LONG_NAME_LEN + MIR_SHORT_NAME_LEN];
+        char task_of[MIR_SHORT_NAME_LEN];
+        sprintf(task_of, "%p", worker->current_task->func);
+        sprintf(schedule_file_name, "%s/loop_%lu_%lu.schedule_opt",
+                runtime->precomp_schedule_dir,
+                strtol(task_of, NULL, 16),
+                idle_join);
 
-                        schedule->next = loop->precomp_schedule;
-                        loop->precomp_schedule = schedule;
-                    }
-                }
-                if (ferror(fp)) {
-                    MIR_LOG_ERR("Error occured while reading precomputed schedule file: %s.",
-                                schedule_file_name);
-                }
-                else if (retval != EOF) {
-                    // Skip over.
-                    fscanf(fp, "%*[^\n]");
-                }
-            }
-            fclose(fp);
-        }
-        else {
+        FILE* fp = fopen(schedule_file_name, "r");
+        if (fp == NULL) {
             goto no_percomputed_schedule;
         }
+        int num_expect = 4;
+        int retval;
+        if (worker->id == 0) {
+            MIR_LOG_INFO("Using precomputed schedule file: %s.",
+                    schedule_file_name);
+        }
+        unsigned long chunk_start, chunk_end, cpu_id, work_cycles;
+        while (!feof(fp)) {
+            while (num_expect == (retval = fscanf(fp,
+                                      "%lu,%lu,%lu,%lu\n",
+                                      &chunk_start,
+                                      &chunk_end,
+                                      &cpu_id,
+                                      &work_cycles))) {
+                loop->precomp_schedule_exists = true;
+                if (cpu_id == MIR_IMPOSSIBLE_CPU_ID) {
+                    MIR_LOG_ERR("Precomputed schedule in file %s uses unsupported MIR_IMPOSSIBLE_CPU_ID.",
+                                schedule_file_name);
+                }
+                else if (cpu_id > runtime->num_workers) {
+                    MIR_LOG_ERR("Precomputed schedule in file %s has more workers than available.",
+                                schedule_file_name);
+                }
+                if (cpu_id == worker->cpu_id) {
+                    struct mir_loop_schedule_t* schedule = mir_malloc_int(sizeof(struct mir_loop_schedule_t));
+                    MIR_CHECK_MEM(schedule != NULL);
+                    schedule->chunk_start = chunk_start;
+                    schedule->chunk_end = chunk_end;
+                    schedule->cpu_id = cpu_id;
+                    schedule->work_cycles = work_cycles;
+
+                    schedule->next = loop->precomp_schedule;
+                    loop->precomp_schedule = schedule;
+                }
+            }
+            if (ferror(fp)) {
+                MIR_LOG_ERR("Error occured while reading precomputed schedule file: %s.",
+                            schedule_file_name);
+            }
+            else if (retval != EOF) {
+                // Skip over.
+                fscanf(fp, "%*[^\n]");
+            }
+        }
+        fclose(fp);
     }
     else {
         goto no_percomputed_schedule;
