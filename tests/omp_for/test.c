dc85453fc6bf655153e47f5646f89796beab3951
Peter A. Jonsson <jonsson.peter.a@gmail.com>
2015-09-02 19:44:41 +0200

    Test for without schedule.

--- tests/omp_for/test.c
+++ tests/omp_for/test.c
@@ -2,6 +2,52 @@
 #include <check.h>
 #include <omp.h>
 
+START_TEST(omp_parallel_for)
+{/*{{{*/
+    int a[128] = {0};
+
+#pragma omp parallel for shared(a)
+    for(int i=0; i<128; i++)
+    {
+        a[i] = i;
+    }
+
+    int sum = 0;
+    int sum_gold = 0;
+    for(int i=0; i<128; i++)
+    {
+        sum += a[i];
+        sum_gold += i;
+    }
+    ck_assert_int_eq(sum, sum_gold);
+}/*}}}*/
+END_TEST
+
+START_TEST(omp_parallel_for_num_threads_small)
+{/*{{{*/
+    int a[128] = {0};
+    int num_threads_reqd = 2;
+    int num_threads;
+
+#pragma omp parallel for shared(a) num_threads(num_threads_reqd)
+    for(int i=0; i<128; i++)
+    {
+        a[i] = i;
+        num_threads = omp_get_num_threads();
+    }
+
+    int sum = 0;
+    int sum_gold = 0;
+    for(int i=0; i<128; i++)
+    {
+        sum += a[i];
+        sum_gold += i;
+    }
+    ck_assert_int_eq(sum, sum_gold);
+    ck_assert_int_eq(num_threads, num_threads_reqd);
+}/*}}}*/
+END_TEST
+
 START_TEST(omp_parallel_for_static)
 {/*{{{*/
     int a[128] = {0};
@@ -1043,6 +1089,9 @@ Suite* test_suite(void)
     Suite* s = suite_create("Test");
 
     TCase* tc = tcase_create("omp_for");
+
+    tcase_add_test(tc, omp_parallel_for);
+    tcase_add_test(tc, omp_parallel_for_num_threads_small);
 
     tcase_add_test(tc, omp_parallel_for_static);
     tcase_add_test(tc, omp_parallel_for_static_num_threads_small);
