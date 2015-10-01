0b8f1b73e757f0d052849db65e222f2bdad714a9
Ananya Muddukrishna <ananya@kth.se>
2015-10-01 16:56:33 +0200

    Added GPL license to libgomp interface implementation.
    - TODO: Move these files out to a seperate location. Also possible is to
      move them to a sub-directory here. After moving, add a copy of the GNU GPL
      license file.

--- src/mir_omp_int.h
+++ src/mir_omp_int.h
@@ -1,3 +1,20 @@
+/*
+This file is part of MIR OpenMP Interface.
+
+MIR OpenMP Interface is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+MIR OpenMP Interface is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with MIR OpenMP Interface.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef MIR_OMP_INT_H
 #define MIR_OMP_INT_H
 
